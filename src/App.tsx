f015070cdec66db852adca39df148640890a057a
adriangut <gut.adrian.95@gmail.com>
2021-05-10 08:35:23 +0200

    display Favourites as an aside to main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Stack } from 'office-ui-fabric-react';
 import { Caller } from './components/Caller';
+import { Favourites } from './components/Favourites';
 
 const App = () => (
-    <Stack horizontal={true} tokens={{ padding: 20, childrenGap: 20 }} horizontalAlign="center">
+    <Stack horizontal={true} tokens={{ padding: 20, childrenGap: 20 }} horizontalAlign="center" verticalAlign="start">
+        <Stack.Item align="center">
+            <Favourites />
+        </Stack.Item>
         <Caller />
     </Stack>
 );
