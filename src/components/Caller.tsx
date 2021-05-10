af0ed6b81b0d489271376c439e8aaabe77881e91
adriangut <gut.adrian.95@gmail.com>
2021-05-10 08:32:31 +0200

    add language selection to main searchbar, add "add favourite" button mock

--- src/components/Caller.tsx
+++ src/components/Caller.tsx
@@ -1,31 +1,41 @@
 import React, { useCallback, useState } from 'react';
 import { getForecast } from '../clients/weather';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
-import { Stack, TextField } from 'office-ui-fabric-react';
+import { ComboBox, IComboBoxOption, IComboBoxProps, IconButton, Stack, TextField } from 'office-ui-fabric-react';
 import { useAsync, useThrottle } from 'react-use';
+import { LANGS_DICT, LANGUAGE_KEYS } from '../utils/langs';
+import { Charts } from './Charts';
+
+const LANGS_OPTIONS: IComboBoxOption[] = Object.entries(LANGS_DICT).map(([lang, langName]) => ({ key: lang, text: langName }));
 
 export const Caller = () => {
     const [search, setSearch] = useState('');
+    const [lang, setLang] = useState<LANGUAGE_KEYS>();
+    const handleLangChange: IComboBoxProps['onChange'] = useCallback(
+        (_ev, option) => setLang(option?.key ? String(option?.key) as LANGUAGE_KEYS : undefined),
+        [],
+    );
     const setSearchValue = useCallback((_ev, value) => setSearch(value), []);
     const throttledSearch = useThrottle(search);
-    const forecast = useAsync(async () => await getForecast(throttledSearch), [throttledSearch]);
+    const forecast = useAsync(async () => {
+        if (!throttledSearch) return undefined;
+        return await getForecast(throttledSearch, lang);
+    }, [throttledSearch, lang ?? 'en']);
 
     return (
         <Stack tokens={{ childrenGap: 20 }}>
-            <TextField
-                defaultValue={search}
-                onChange={setSearchValue}
-                placeholder="Start typing your area of interest..."
-            />
+            <Stack grow={true} horizontal={true} tokens={{ childrenGap: 15 }}>
+                <Stack grow={4}>
+                    <TextField
+                        defaultValue={search}
+                        onChange={setSearchValue}
+                        placeholder="Start typing your area of interest..."
+                    />
+                </Stack>
+                <ComboBox placeholder="Select language..." options={LANGS_OPTIONS} selectedKey={lang} onChange={handleLangChange} />
+                <IconButton iconProps={{ iconName: 'plus' }} label="Add favourite" />
+            </Stack>
 
-            <LineChart width={600} height={300} data={(forecast?.value as any)?.list}>
-                <Line type="natural" dataKey="main.temp" stroke="#8884d8" />
-                <CartesianGrid stroke="#F3F2F1" />
-                <Tooltip />
-                <Legend />
-                <XAxis dataKey="dt_txt" />
-                <YAxis />
-            </LineChart>
+            <Charts width={600} height={300} data={forecast.value?.list} />
         </Stack>
     );
 }
