ccf66bc3372ae58f96d7ea775373dadc094c9db0
adriangut <gut.adrian.95@gmail.com>
2021-05-10 08:31:28 +0200

    add language support to forecast request, ask for data in  metric format

--- src/clients/weather.ts
+++ src/clients/weather.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import { GetForecastResponse } from '../types/weather.types';
+import { LANGUAGE_KEYS } from '../utils/langs';
 
 const client = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5/',
-    params: { appId: process.env.REACT_APP_API_KEY },
+    params: { appId: process.env.REACT_APP_API_KEY, units: 'metric' },
 });
 
-export const getForecast = async (city: string) => (await client.get(
+export const getForecast = async (city: string, lang: LANGUAGE_KEYS = 'en') => (await client.get<GetForecastResponse>(
     'forecast',
-    { params: { q: city } },
+    { params: { q: city, lang } },
 )).data;
